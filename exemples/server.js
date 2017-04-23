5c020dfda87fcfdf9959311fe5abe5f459754d9c
SurionA <frere.maxime@gmail.com>
2017-04-23 15:29:39 +0200

    #UPDATE# add exemple

---
+++ exemples/server.js
@@ -0,0 +1,14 @@
+const http = require('http');
+const fs = require('fs');
+const server = http.createServer((req, res) => {
+    fs.readFile('./index.html', 'utf-8', function(error, content) {
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(content);
+    });
+});
+const io = require('socket.io').listen(server);
+
+server.listen(3000);
+const radioFrequenceControl = require('../index');
+
+radioFrequenceControl.initSocket(io);
